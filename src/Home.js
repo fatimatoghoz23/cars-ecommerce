ab8bfaef5bedbf0d4bb5f06bbf2da4ac39ed77af
fatimatoghoz23 <140913550+fatimatoghoz23@users.noreply.github.com>
2024-03-11 12:35:14 +0300

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Main from './component/main/Main';
+import SSlider from './component/Slider/Slider'
+import Categories from './component/Categories/Categories';
+import Footer from './component/footer/Footer';
+function Home() {
+  return (
+    <div>
+    <Main/>
+    <SSlider/>
+    <Categories/>
+    <Footer/>
+
+    </div>
+  )
+}
+
+export default Home
