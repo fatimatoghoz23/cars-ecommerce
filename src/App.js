8d0f440c6a4c6b4a9461d627acfc68c423706ede
fatimatoghoz23 <140913550+fatimatoghoz23@users.noreply.github.com>
2024-03-13 15:57:48 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './component/Header/Header';
-import {HashRouter as Router,Routes ,Route} from 'react-router-dom'
-import Cart from './component/Cart/Cart';
-import Home from './Home';
-import Product from './component/Product/Product';
+import {BrowserRouter as Router,Routes ,Route} from 'react-router-dom'
+import Cart from './Pages/Cart/Cart';
+import Main from './Pages/main/Main';
+import Product from './Pages/Product/Product';
 function App() {
   
   return(
@@ -12,7 +12,7 @@ function App() {
      <Header/>
     <Router>
       <Routes>
-      <Route exact Component={Home} path='/' />
+      <Route exact Component={Main} path='/' />
       <Route exact Component={Cart} path='/Cart' />
       <Route exact Component={Product} path='/Product' />
       </Routes>
