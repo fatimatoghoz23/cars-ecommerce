9d1273855e5fd21e14000840a8c55ea0082f9159
fatimatoghoz23 <140913550+fatimatoghoz23@users.noreply.github.com>
2024-03-13 16:01:06 +0300

    Update Categories.jsx

--- src/component/Categories/Categories.jsx
+++ src/component/Categories/Categories.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './Categories.css'
 import { Link, useParams } from 'react-router-dom';
-import Product from '../Product/Product';
 import { GoArrowRight } from "react-icons/go";
 const data =[
   {
