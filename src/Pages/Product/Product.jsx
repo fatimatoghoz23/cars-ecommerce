282269cdfba05f86c095b8479362d773de7c8a01
fatimatoghoz23 <140913550+fatimatoghoz23@users.noreply.github.com>
2024-03-25 16:24:33 +0300

    Update Product.jsx

--- src/Pages/Product/Product.jsx
+++ src/Pages/Product/Product.jsx
@@ -1,64 +1,67 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Categories from '../../component/Categories/Categories';
 import './Product.css'
 import { Link, useParams } from 'react-router-dom';
-import {FaStar} from 'react-icons/fa'
+import {FaCheck, FaStar} from 'react-icons/fa'
 import { useState } from 'react';
 import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
 import "swiper/css"
 import "swiper/css/free-mode"
 import "swiper/css/thumbs"
 import {FreeMode,Navigation,Thumbs} from "swiper/modules"
-const data =[
-  {
-    name:"Audi",
-    image:"./picture22.png",
-    color:'Black',
-    price:'$240.55'
+import { Cartcontext } from '../../Context';
+const data =
+ [ {
+  id:'1',
+  name:"AUDI",
+  image:"/picture22.png",
+  color:['#6799D5','#000000','#ffeeff'],
+  price:240.55,
+  qty:1
   },
   {
+    id:'2',
     name:"Honda",
-    image:"./picture2.png",
-    color:'Black',
-    price:'$240.55'
+    image:"/picture33.png",
+    color:['#6799D5','#000000','#ffeeff'],
+    price:120.55,
+    qty:1
   },
    {
+    id:'3',
+    name:"VOLVO",
+    image:"/picture44.png"
+    , color:['#6799D5','#000000','#ffeeff'],
+    price:210.55,
+    qty:1
+  },
+   {
+    id:'4',
     name:"BMW",
-    image:"./picture55.png",
-    color:'Black',
-    price:'$240.55'
-  },
-   {
-    name:"Volvo",
-    image:"./picture44.png",
-    color:'Black',
-    price:'$240.55'
-  },
-  {
-    name:"Volvo",
-    image:"./picture44.png",
-    color:'Black',
-    price:'$240.55'
-  },
-
+    image:"/picture55.png",
+    color:['#6799D5','#000000','#ffeeff'],
+    price:640.55,
+    qty:1
+  }]
   
-]
-const img=['./ss.png','./Rectangle 47.png','./Rectangle 48.png','./Rectangle 49.png']
-const color=['#6799D5','#000000','#ffeeff']
+const img=['/ss.png','/Rectangle 47.png','/Rectangle 48.png','/Rectangle 49.png']
+// const color=['#6799D5','#000000','#ffeeff']
 function Product() {
   const[swiper,setSwiper]=useState(null)
-  const[number,setNumber]=useState(1)
   const [rating,setRating]=useState(null)
   const [hover,setHover]=useState(null)
-  // const { productId } = useParams();
-  // const product = Categories.find((product) => product.id === productId);
-  // const { image, name } = product;
-  
+  const { productId } = useParams();
+  const product = data.find((product) => product.id === productId);
+  const { image, name,price ,qty,color } = product;
+  const[colors,setColors]=useState(color[0])
+  const Globalstate = useContext(Cartcontext);
+  const dispatch = Globalstate.dispatch;
   return (
     <div>
     <div className="flexCenter m-container">
      <div className="product-left paddings">
       <div className="product-details  innerWidth">
-        <h2>Honda-Civic Type R</h2>
+        <h2>{name}</h2>
         <p>6000 cc</p>
         <div className="flexCenter">
         <Swiper 
@@ -70,7 +73,7 @@ function Product() {
         (
         <SwiperSlide>
         <div className='image-slider flexCenter '>
-        <img src={d} width={400} height={300} />
+        <img src={image} width={400} height={300} />
         </div> 
         </SwiperSlide>
         )
@@ -125,18 +128,24 @@ function Product() {
             </p>
             <h2>Colors</h2>
             <div className="color ">
-              {color.map((color,i)=>(
-                <button key={i} style={{backgroundColor:color}}  className='btn-style' ></button>
+              {color.map((c,i)=>(
+                <button key={i} style={{backgroundColor:c}} 
+                 className={ color===c? 'btn-style active':'btn-style' }
+                 onClick={()=>setColors(c)}
+                 >
+                  {colors === c ?<FaCheck className='checkStyle'/> :null}
+                </button>
               ))}
             </div>
+            
             <div className="price ">
-            <img onClick={()=>number>1?setNumber(number-1):setNumber(1)} src='./min.png' width={20} height={20}/>
-           <p>{number}</p>
-            <img onClick={()=>setNumber(number+1)} src='./plus.png' width={20} height={20} />
-            <h2>Price:<span>250$</span></h2>
+            <img  onClick={() => dispatch({ type: "INCREASE", payload: product })} src='./min.png' width={20} height={20}/>
+           <p>{qty}</p>
+            <img   onClick={() => dispatch({ type: "INCREASE", payload: product })} src='./plus.png' width={20} height={20} />
+            <h2>Price:<span>{price}$</span></h2>
             </div>
             <div className="button-p">
-            <button className='add'>Add To Cart</button>
+            <button className='add'  onClick={() => dispatch({ type: "ADD", payload: product })}>Add To Cart</button>
             <button className='buy2'>Buy Now</button>
             </div>
             </div>
